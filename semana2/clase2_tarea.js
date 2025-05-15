6dfd5ea0e02170a9d0c0bea0eb6d31edbf4bf574
PanDeElote <verdisa17@gmail.com>
2025-05-15 14:51:23 -0600

    Tarea 2 y notas en el ejercicio 2

--- semana2/clase2_tarea.js
+++ semana2/clase2_tarea.js
@@ -11,3 +11,52 @@
 // 2. Eliminar fruta
 // 3. Ver lista de frutas
 // 4. Salir
+
+const { ask } = require('../helpers/input');
+
+async function main(){
+
+    let frutas = [];
+    let option = Number;
+
+    while ( option !== 4){
+        option = Number(await ask('Qué deseas hacer? \n1)Agregar fruta \n2)Eliminar fruta \n3)Ver lista de frutas \n4)Salir \n'));
+
+        if ( option == 1){
+            let frutaNueva = await ask('Escribe el nombre de la fruta que deseas agregar: ');
+            frutas.push(frutaNueva);
+            console.log(`${frutaNueva} ha sido agregada a la lista exitosamente.`);
+            console.log('Si deseas ver la lista de frutas, oprime 2.');
+        } else if ( option == 2){
+            if (frutas.length == 0){
+                console.log('No hay frutas disponibles.');
+            } else {
+                let eliminarFruta = await ask('Qué fruta deseas eliminar? ');
+                let index = frutas.indexOf(eliminarFruta);
+
+                if(index == -1){
+                    console.log('La fruta no existe en el arreglo. No se puede eliminar.');
+                } else{
+                    frutas.splice(index,1);
+                    console.log(`${eliminarFruta} ha sido eliminada con éxito.`);
+                }
+            }
+        } else if(option == 3){
+            console.log(`${frutas}`);
+            continue;
+        } else if (option == 4){
+            let salirSiNo = await ask('Estás seguro de querer salir del programa? Si o No?');
+            if (salirSiNo === "No"){
+                console.log('Presione otra opción del menú.');
+                return; //DUDAAAAAAA//
+            } else{
+                console.log('Gracias por usar el programa!');
+            }
+        } else{
+            console.log('Opción no válida.')
+        }
+
+    }
+}
+
+main();
--- semana2/ejercicio2.js
+++ semana2/ejercicio2.js
@@ -1,28 +1,28 @@
 const { ask } = require('../helpers/input');
 
 async function main() {
-  let opcion = '';
-  let frutas = [];
+  let opcion = ''; //Me dice que la variable es un dato STRING//
+  let frutas = []; //Dice que frutas es UN ARREGLO VACÍO//
 
   while (opcion !== '3') {
     opcion = await ask('¿Qué deseas hacer? \n1. Agregar una fruta \n2. Eliminar una fruta \n3. Salir\n');
     if (opcion === '1') {
       //Agregar
       let frutaNueva = await ask('¿Qué fruta deseas agregar?: ');
-      frutas.push(frutaNueva);
+      frutas.push(frutaNueva); //Agrega el coso al arreglo (Le haces el push a frutaNueva)//
       console.log(`Fruta agregada: ${frutaNueva}`);
       console.log('Frutas disponibles: ', frutas);
     } else if (opcion === '2') {
       //Eliminar
       if (frutas.length === 0) {
         console.log('No hay frutas disponibles');
-        continue;
+        continue; //Para SALIR DEL IF y seguir con la wea esta//
       } else {
         let frutaEliminar = await ask('¿Qué fruta deseas eliminar?: ');
-        let index = frutas.indexOf(frutaEliminar);
+        let index = frutas.indexOf(frutaEliminar); //Provee la POSICION delobjeto que buscas//
 
-        if (index !== -1) {
-          frutas.splice(index, 1);
+        if (index !== -1) {  // -1 quiere decir que no está en el arreglo //
+          frutas.splice(index, 1); //BORRAR ALGO DEL ARREGLO (posicion, cúantos quieres borrar a partir de ahí y contandose a sí mismo) //
           console.log(`${frutaEliminar} ha sido eliminada `)
         } else {
           console.log('Fruta no encontrada ');
