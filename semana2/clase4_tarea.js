0f44b1688979583be376a32d0aab2ae1e85dde6b
PanDeElote <verdisa17@gmail.com>
2025-05-19 21:48:47 -0600

    Tarea 4

--- semana2/clase4_tarea.js
+++ semana2/clase4_tarea.js
@@ -12,3 +12,58 @@
 //Promedio general
 //
 //La calificación más alta y la más baja
+
+const { ask } = require('../helpers/input');
+
+function analizarCalificaciones(numeros){
+    let cantidadDeAprobados = 0;
+    let cantidadDeReprobados = 0;
+    let sumaTotal = 0;
+    let menor = numeros[0];
+    let mayor = numeros[0];
+
+    for (let i = 0; i < numeros.length; i++){
+        if( numeros[i] >= 70){
+            cantidadDeAprobados++;
+        } else{
+            cantidadDeReprobados++;
+        }
+
+        if(numeros[i] < menor){
+            menor = numeros[i];
+        }
+        if(numeros[i] > mayor){
+            mayor = numeros[i];
+        }
+
+        sumaTotal = sumaTotal + numeros[i];
+    }
+
+    promedio = sumaTotal / numeros.length;
+
+    return {cantidadDeAprobados, cantidadDeReprobados, promedio, menor, mayor};
+
+}
+
+//NOTAAAAA: A VECES ES MEJOR HACER LAS FUNCIONES POR SEPARADO
+
+async function main(){
+    let numDeCalificaciones = Number(await ask('Ingrese la cantidad de calificaciones a analizar: '));
+    let calificaciones = [];
+    console.log('Escriba la lista de calificaciones a continuación: ')
+
+    for(let i = 0; i < numDeCalificaciones; i++){
+        let calificacionNueva = Number(await ask(`Ingrese la calificación ${i + 1}: `));
+        if((calificacionNueva <= 100) && (calificacionNueva >= 0)){
+            calificaciones.push(calificacionNueva);
+        } else{
+            console.log('Este número no es válido. Ingresa otro.')
+            i--;
+        }
+    }
+    console.log(calificaciones);
+
+    const analisis = analizarCalificaciones(calificaciones);
+    console.log(`De la lista de calificaciones la información obtenida es: \nCantidad de aprobados: ${analisis.cantidadDeAprobados}\nCantidad de reprobados: ${analisis.cantidadDeReprobados}\nPromedio del arreglo: ${analisis.promedio}\nCalificación más baja: ${analisis.menor}\nCalificación más alta: ${analisis.mayor}`);
+}
+main();
