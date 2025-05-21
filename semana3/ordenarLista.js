b3cf6be95277fb3f0996ab66076ada0c41c80fda
PanDeElote <verdisa17@gmail.com>
2025-05-21 13:00:33 -0600

    Notas extra

--- semana3/ordenarLista.js
+++ semana3/ordenarLista.js
@@ -1,8 +1,13 @@
 const { ask } = require('../helpers/input');
 
 function ordenarLista(numeros){
-   return numeros.sort((a, b) => b - a);
+   return numeros.sort((a, b) => b - a); //El SORT es para ordenar listasssss DE MAYOR A MENOR//
+}
+
+function deMenorMayor(numeros){
+    return numeros.sort((a, b) => a - b);//De MENOR A MAYOR//
 }
 
 const listita = [12, 45, 45678, 87654, 444];
 console.log(ordenarLista(listita));
+console.log(deMenorMayor(listita));
